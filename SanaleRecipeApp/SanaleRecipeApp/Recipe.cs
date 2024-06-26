58e6fb16e8d265887ac4970b14a7b698eab641a0
Jose Gonzalves <st10194321@vcconnect.edu.za>
2024-06-26 19:54:12 +0200

    Commit 1
    Created a WPF application, I have created a new class called Recipe in which I have imported my properties from part 2.

---
+++ SanaleRecipeApp/SanaleRecipeApp/Recipe.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanaleRecipeApp
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
+        public List<string> Steps { get; set; } = new List<string>();
+    }
+    public class Ingredient
+    {
+        public string Name { get; set; }
+        public double Quantity { get; set; }
+        public string Unit { get; set; }
+        public int Calories { get; set; }
+        public string FoodGroup { get; set; }
+        public double OriginalQuantity { get; set; }
+        public int OriginalCalories { get; set; }
+
+        public Ingredient(string name, double quantity, string unit, int calories, string foodGroup)
+        {
+            Name = name;
+            Quantity = quantity;
+            OriginalQuantity = quantity;
+            Unit = unit;
+            Calories = calories;
+            OriginalCalories = calories;
+            FoodGroup = foodGroup;
+        }
+       
+    }
+    public delegate void CalorieNotification(string recipeName);
+}
